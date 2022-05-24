e016a8207f11afb92495354a75cb693c98c80ac6
Shahzain Arshad <92491226+kyrorb@users.noreply.github.com>
2022-05-24 07:45:21 +0000

    Added Basic Question Retrieval

--- src/ctrl.py
+++ src/ctrl.py
@@ -1,1 +1,31 @@
 ## ctrl
+
+print("test")
+
+from pathlib import Path
+import random
+
+def grab_question():
+
+  questions = []
+  
+  file = Path('papers/edexcel')
+  paper_q = ""
+  
+  for entry in file.iterdir():
+    
+    if str(entry).endswith(".txt"):
+      paper_q = str(entry)
+
+  file = open(paper_q)
+  questions_raw = file.readlines()
+
+  for question_unf in questions_raw:
+
+    if question_unf.endswith("")
+  
+  return questions_raw
+  
+
+grab_question()
+  
